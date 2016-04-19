35f3eff43e9d2d0e7ebb435e2422ed6494d21840
maticoh <matic.hajsen@gmail.com>
2016-04-19 20:53:28 +0200

    Started work on creating bases. Creates entities so far.

--- data-retrieval-parsing/database.py
+++ data-retrieval-parsing/database.py
@@ -23,12 +23,42 @@ class Database:
         self.conn.close()
 
     def create_tables(self):
-        # TODO
-        pass
+        #TODO
+
+        author = "author (author_id INTEGER PRIMARY KEY, location TEXT, birthday DATE, date_joined DATE NOT NULL)"
+        fandom = "fandom (fandom_id INTEGER PRIMARY KEY, name TEXT NOT NULL)"
+        warning = "warning (warning_id INTEGER PRIMARY KEY, description TEXT NOT NULL)"
+        category = "category (category_id INTEGER PRIMARY KEY, name TEXT)"
+        character = "character (character_id INTEGER PRIMARY KEY, name TEXT)"
+        story = "story (story_id INTEGER PRIMARY KEY, completed BOOLEAN NOT NULL, word INTEGER NOT NULL, summary TEXT NOT NULL, rating TEXT NOT NULL, hits INTEGER NOT NULL, kudos INTEGER NOT NULL, title TEXT NOT NULL, language TEXT NOT NULL, chapters INTEGER NOT NULL, comments INTEGER NOT NULL)" 
+
+        entities = [author, fandom, category, character, story]
+        
+        cur = self.conn.cursor()
+        for entity in entities:
+            ukaz = "CREATE TABLE IF NOT EXISTS " + entity
+            cur.execute(ukaz)
+        
+        self.conn.commit()
+
+
+        
 
     def insert_story(self, story):
         # TODO
         pass
+
+    def lil_bobby(self, tekst):
+        cur = self.conn.cursor()
+        ukaz = "DROP TABLE " + tekst
+        cur.execute(ukaz)
+##        conn_string = "host='{0}' dbname='{1}' user='{2}' password='{3}'".format(self.host, self.db_name,
+##                                                                                 self.user, self.password)
+##        with psycopg2.connect(conn_string) as con:
+##            cur = con.cursor()
+##            ukaz = "DROP TABLE " + tekst
+##            cur.execute(ukaz)
+        cur.close()
 
     def test(self):
         cursor = self.conn.cursor()
@@ -37,3 +67,5 @@ class Database:
         for row in rows:
             print(row)
         cursor.close()
+
+
