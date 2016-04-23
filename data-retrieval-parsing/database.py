89e5662a521f89bbe81e2ac2552a59794d2c773b
maticoh <matic.hajsen@gmail.com>
2016-04-23 13:19:44 +0200

    Started work on creating relationship tables.

--- data-retrieval-parsing/database.py
+++ data-retrieval-parsing/database.py
@@ -25,18 +25,71 @@ class Database:
     def create_tables(self):
         # IN PROGRESS
 
-        author = "author (author_id INTEGER PRIMARY KEY, location TEXT, birthday DATE, date_joined DATE NOT NULL)"
-        fandom = "fandom (fandom_id INTEGER PRIMARY KEY, name TEXT NOT NULL)"
-        warning = "warning (warning_id INTEGER PRIMARY KEY, description TEXT NOT NULL)"
-        category = "category (category_id INTEGER PRIMARY KEY, name TEXT)"
-        character = "character (character_id INTEGER PRIMARY KEY, name TEXT)"
-        story = "story (story_id INTEGER PRIMARY KEY, completed BOOLEAN NOT NULL, word INTEGER NOT NULL, summary TEXT NOT NULL, rating TEXT NOT NULL, hits INTEGER NOT NULL, kudos INTEGER NOT NULL, title TEXT NOT NULL, language TEXT NOT NULL, chapters INTEGER NOT NULL, comments INTEGER NOT NULL, date_published DATE NOT NULL)" 
-        entities = [author, fandom, category, character, story]     
-        
+        author = "author (" \
+                 "author_id INTEGER PRIMARY KEY" \
+                 ", location TEXT, birthday DATE" \
+                 ", date_joined DATE NOT NULL" \
+                 ")"
+        fandom = "fandom (" \
+                 "fandom_id INTEGER PRIMARY KEY" \
+                 ", name TEXT NOT NULL" \
+                 ")"
+        warning = "warning (" \
+                  "warning_id INTEGER PRIMARY KEY," \
+                  " description TEXT NOT NULL" \
+                  ")"
+        category = "category (" \
+                   "category_id INTEGER PRIMARY KEY," \
+                   " name TEXT" \
+                   ")"
+        character = "character (" \
+                    "character_id INTEGER PRIMARY KEY, " \
+                    "name TEXT)"
+        story = "story (" \
+                "story_id INTEGER PRIMARY KEY, " \
+                "completed BOOLEAN NOT NULL, word INTEGER NOT NULL, " \
+                "summary TEXT NOT NULL, " \
+                "rating TEXT NOT NULL, " \
+                "hits INTEGER NOT NULL, " \
+                "kudos INTEGER NOT NULL, " \
+                "title TEXT NOT NULL, " \
+                "language TEXT NOT NULL, " \
+                "chapters INTEGER NOT NULL, " \
+                "comments INTEGER NOT NULL, " \
+                "date_published DATE NOT NULL, " \
+                "written_by INTEGER NOT NULL REFERENCES author(author_id)" \
+                ")"
+
+        contains_fandom = "contains_fandom(" \
+                          "story INTEGER NOT NULL REFERENCES story(story_id), " \
+                          "fandom INTEGER NOT NULL REFERENCES fandom(fandom_id) " \
+                          ")"
+        has_warning = "has_warning(" \
+                      "story INTEGER NOT NULL REFERENCES story(story_id), " \
+                      "warning INTEGER NOT NULL REFERENCES warning(warning_id) " \
+                      ")"
+        is_in_category = "is_in_category( " \
+                         "story INTEGER NOT NULL REFERENCES story(story_id), " \
+                         "category INTEGER REFERENCES category(category_id) " \
+                         ")"
+        contains_characters = "contains_characters(" \
+                              "story INTEGER NOT NULL REFERENCES story(story_id), " \
+                              "character INTEGER REFERENCES character(character_id)" \
+                              ")"
+        relationship = "relationship(" \
+                       "person_1 INTEGER NOT NULL REFERENCES character(character_id), " \
+                       "person_2 INTEGER NOT NULL REFERENCES character(character_id), " \
+                       "PRIMARY KEY (person_1, person_2)" \
+                       ")"
+
+        tables = [author, fandom, category, character, story, warning,
+                  contains_fandom, has_warning, is_in_category, contains_characters, relationship]
+
+
         cursor = self.conn.cursor()
-        for entity in entities:
-            ukaz = "CREATE TABLE IF NOT EXISTS " + entity
-            cursor.execute(ukaz)
+        for table in tables:
+            query = "CREATE TABLE IF NOT EXISTS " + table
+            cursor.execute(query)
         self.conn.commit()
         cursor.close()
 
@@ -47,18 +100,6 @@ class Database:
         # TODO
         pass
 
-    def lil_bobby(self, tekst):
-        cur = self.conn.cursor()
-        ukaz = "DROP TABLE " + tekst
-        cur.execute(ukaz)
-##        conn_string = "host='{0}' dbname='{1}' user='{2}' password='{3}'".format(self.host, self.db_name,
-##                                                                                 self.user, self.password)
-##        with psycopg2.connect(conn_string) as con:
-##            cur = con.cursor()
-##            ukaz = "DROP TABLE " + tekst
-##            cur.execute(ukaz)
-        cur.close()
-
     def test(self):
         cursor = self.conn.cursor()
         cursor.execute("SELECT * FROM Cars")
@@ -66,5 +107,3 @@ class Database:
         for row in rows:
             print(row)
         cursor.close()
-
-
