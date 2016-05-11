084f5245bd25209c03762ffe4c01a6d114983d7b
Ines Meršak <inesmersak@gmail.com>
2016-05-11 14:13:08 +0200

    Tried to get all character names from database.

--- shiny-application/server.R
+++ shiny-application/server.R
@@ -5,21 +5,25 @@ library(RPostgreSQL)
 source("auth.R")
 
 shinyServer(function(input, output) {
-  # Vzpostavimo povezavo
+  # connect to database
   conn <- src_postgres(dbname = db, host = host,
                        user = user, password = password)
-  # Pripravimo tabelo
+  # select table
   tbl.authors <- tbl(conn, "author")
 
   output$authors <- renderTable({
-    # Naredimo poizvedbo
-    # x %>% f(y, ...) je ekvivalentno f(x, y, ...)
+    # filter and order table data
     t <- tbl.authors %>% filter(author_id > input$min) %>%
       arrange(username) %>% data.frame()
     t$date_joined <- as.character(t$date_joined)
     t$birthday <- as.character(t$birthday)
-    # Vrnemo dobljeno razpredelnico
+    
+    # return the table
     t
   })
+  
+   tbl.characters <- tbl(conn, "character")
+   tbl.characters %>% arrange(name) %>% select(name)
+  
 
 })
